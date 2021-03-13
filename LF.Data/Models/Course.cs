6232cf71c850ee6b17f0b8084e21240e4597c21c
Szikistar <szikistar@google.com>
2021-03-14 00:24:14 +0100

    #4: Course.Equals implementáció törlése

--- LF.Data/Models/Course.cs
+++ LF.Data/Models/Course.cs
@@ -1,39 +1,8 @@
-using System;
-
 namespace LF.Data.Models
 {
-    public class Course : IEquatable<Course>
+    public class Course
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        public override bool Equals(object obj) => Equals(obj as Course);
-
-        public bool Equals(Course course)
-        {
-            if(null == course)
-            {
-                return false;
-            }
-
-            if(Id != course.Id || Name != course.Name)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            
-            unchecked
-            {
-                int hash = 27;
-                hash = (hash * 13) + Id.GetHashCode();
-                hash = (hash * 13) + Name.GetHashCode();
-                return hash;
-            }
-        }
     }
 }
